7b79a7d1844542612237bc5668dd2876dc6f7c77
Billy Bao <newyorkupperbay@gmail.com>
2025-01-13 11:08:45 +0000

    main

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# MapCoder
+MapCoder.py = Original
+MapCoder_v1.py = Introduce a project manager to deliver task and allocate to appropriate worker
+MapCoder_test.py = Functionality test for small group of agent
---
+++ src/promptings/PromptingFactory.py
@@ -0,0 +1,29 @@
+from promptings.CoT import CoTStrategy
+from promptings.Direct import DirectStrategy
+from promptings.Analogical import AnalogicalStrategy
+from promptings.SelfPlanning import SelfPlanningStrategy
+from promptings.MapCoder import MapCoder as MapCoder
+
+from promptings.MapCoder_v1 import MapCoder as MapCoder_test
+from promptings.MapCoder_v2 import MapCoder as MapCoder_v2
+from promptings.MapCoder_without_KB import MapCoder as MapCoder_without_KB
+
+class PromptingFactory:
+    @staticmethod
+    def get_prompting_class(prompting_name):
+        if prompting_name == "CoT":
+            return CoTStrategy
+        elif prompting_name == "MapCoder":
+            return MapCoder
+        elif prompting_name == "Direct":
+            return DirectStrategy
+        elif prompting_name == "Analogical":
+            return AnalogicalStrategy
+        elif prompting_name == "SelfPlanning":
+            return SelfPlanningStrategy
+        elif prompting_name == "MapCoder_test":
+            return MapCoder_v2
+        elif prompting_name == "MapCoder_without_KB":
+            return MapCoder_without_KB
+        else:
+            raise Exception(f"Unknown prompting name {prompting_name}")
